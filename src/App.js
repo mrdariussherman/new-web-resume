f46775f400dc980d372dcc62c0b39e1a5124366d
Darius Sherman <Darius_Sherman@baylor.edu>
2019-06-20 23:56:11 -0500

    add project files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as Resume from './resume.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Resume.Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/resume.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from "./logo.svg";
+
+export class Header extends React.Component {
+    render() {
+        return(
+          <div className="container padded text-center">
+              <header className="App-header">
+                  <code>darius sherman</code>
+              </header>
+          </div>
+        );
+    }
+}
