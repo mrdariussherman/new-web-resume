7f9b712a42f7ba4784834bb43a63dc75924c3061
Darius Sherman <Darius_Sherman@baylor.edu>
2019-06-21 00:01:57 -0500

    Updates

---
+++ precache-manifest.d84cf788fb9025f35d19302aeb2fac1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607f82b68adbb0b514a2540e1784f5e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dbb01b41b6e163e312a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "6dbb01b41b6e163e312a",
+    "url": "/static/js/main.e947196b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
