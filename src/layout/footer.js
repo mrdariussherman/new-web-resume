2272cb9d15505fa12546353e28d922c4f92822b1
Darius Sherman <darius@darius.dev>
2024-07-27 15:46:54 -0500

    footer spacing fix

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -7,19 +7,19 @@ export class Footer extends React.Component {
             <footer className="App-footer align-items-center">
                 <div className="py-2 align-middle text-center">
                     <a target="_blank" rel="noopener noreferrer" href="https://www.twitter.com/mrdariussherman">
-                        <span className="fab fa-2x fa-twitter mr-4"/>
+                        <span className="fab fa-2x fa-twitter mx-2"/>
                     </a>
                     <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/mrdariussherman">
-                        <span className="fab fa-2x fa-facebook mr-4"/>
+                        <span className="fab fa-2x fa-facebook mx-2"/>
                     </a>
                     <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/mrdariussherman">
-                        <span className="fab fa-2x fa-instagram mr-4"/>
+                        <span className="fab fa-2x fa-instagram mx-2"/>
                     </a>
                     <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/dariussherman/">
-                        <span className="fab fa-2x fa-linkedin mr-4"/>
+                        <span className="fab fa-2x fa-linkedin mx-2"/>
                     </a>
                     <a target="_blank" rel="noopener noreferrer" href="https://www.github.com/mrdariussherman">
-                        <span className="fab fa-2x fa-github"/>
+                        <span className="fab fa-2x fa-github mx-2"/>
                     </a>
                 </div>
             </footer>
