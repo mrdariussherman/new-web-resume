e20d4a2ee6b75cf1ce40f9999622b89fd2fbc1b4
Darius Sherman <darius@darius.dev>
2024-07-28 15:11:41 -0500

    remove tooltip from header

--- src/layout/header.js
+++ src/layout/header.js
@@ -4,7 +4,7 @@ export class Header extends React.Component {
     render() {
         return(
             <header className="container-fluid text-center App-header py-4 fixed-top">
-                <label><b>darius sherman</b></label>
+                <b>darius sherman</b>
             </header>
         );
     }
